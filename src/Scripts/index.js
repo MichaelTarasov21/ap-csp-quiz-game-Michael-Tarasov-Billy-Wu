a9048f73c75eaf55a25841adabac83651941d8bc
Michael Tarasov <Michael.Tarasov@tutanota.com>
2020-10-18 17:58:58 -0400

    Added Bootstrap
    Temporarily changed the HTML page for testing of CSS

---
+++ src/Scripts/index.js
@@ -0,0 +1,3 @@
+console.log("Javascript initialized");
+var points = 0;
+document.getElementById("score").innerHTML = `Score:${points}`;
--- src/index.js
+++
@@ -1,2 +0,0 @@
-console.log("Javascript initialized");
-document.getElementById(`row1`).innerHTML = `<button></button>`
