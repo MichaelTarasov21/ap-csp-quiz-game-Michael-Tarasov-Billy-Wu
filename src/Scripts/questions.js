09738adf9d636001c1da40b0d1f1f64a4c4cd229
Michael Tarasov <Michael.Tarasov@tutanota.com>
2020-10-19 16:46:40 -0400

    Expanded the question form
    Added answered boolean and inputed the second question

--- src/Scripts/questions.js
+++ src/Scripts/questions.js
@@ -1,11 +1,21 @@
-const questions = [
+var questions = [
   {
     qprompt: "How many pokemon were in the first game?",
-    c1: "A) 100", 
+    c1: "A) 100",
     c2: "B) 151",
     c3: "C) 150",
     c4: "D) 101",
-    answer: "B"
+    answer: "B",
+    answered: false,
+  },
+  {
+    qprompt: "When was pokemon FIRST released?",
+    c1: "A) 1996",
+    c2: "B) 1997",
+    c3: "C) 1998",
+    c4: "D) 1999",
+    answer: "A",
+    answered: false,
   },
 ];
 export { questions };
