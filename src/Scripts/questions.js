4b97d888dd698effa6561bdaaeb4c3c06d56b495
Michael Tarasov <Michael.Tarasov@tutanota.com>
2020-10-19 16:41:42 -0400

    Created a questions form in js

---
+++ src/Scripts/questions.js
@@ -0,0 +1,11 @@
+const questions = [
+  {
+    qprompt: "How many pokemon were in the first game?",
+    c1: "A) 100", 
+    c2: "B) 151",
+    c3: "C) 150",
+    c4: "D) 101",
+    answer: "B"
+  },
+];
+export { questions };
