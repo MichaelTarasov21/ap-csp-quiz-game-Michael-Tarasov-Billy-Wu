2663c315cbfb71dda862fc91107ef7dc3a0df1df
Michael Tarasov <Michael.Tarasov@tutanota.com>
2020-10-20 16:25:47 -0400

    Stripped code from body and commented out javascript

--- src/Scripts/index.js
+++ src/Scripts/index.js
@@ -1,19 +1,24 @@
-import {questions} from './questions.js';
+import { questions } from "./questions.js";
+function loadquestionselectionpage() {
+    
+  let i;
+  for (i = 0; i < 16; i++) {
 
-console.log(document.getElementById("q1"));
+  }
+}
+
 var points = 0;
 document.getElementById("score").innerHTML = `SCORE:${points}`;
 
-console.log(questions[0])
+//console.log(questions[0])
 
-const Body = document.body
+//const Body = document.body
 
-
-const question = document.getElementById("q1").addEventListener("click", function(){
-    Body.innerHTML = "";
-    Body.insertAdjacentHTML('beforeend', '<ul class = "question">' +questions[0].qprompt+ '</ul>');
-    Body.insertAdjacentHTML('beforeend', '<ul class = "choice">' +questions[0].c1+ '</ul>');
-    Body.insertAdjacentHTML('beforeend', '<ul class = "choice">' +questions[0].c2+ '</ul>');
-    Body.insertAdjacentHTML('beforeend', '<ul class = "choice">' +questions[0].c3+ '</ul>');
-    Body.insertAdjacentHTML('beforeend', '<ul class = "choice">' +questions[0].c4+ '</ul>');
-})
+//const question = document.getElementById("q1").addEventListener("click", function(){
+//    Body.innerHTML = "";
+//    Body.insertAdjacentHTML('beforeend', '<ul class = "question">' +questions[0].qprompt+ '</ul>');
+//    Body.insertAdjacentHTML('beforeend', '<ul class = "choice">' +questions[0].c1+ '</ul>');
+//    Body.insertAdjacentHTML('beforeend', '<ul class = "choice">' +questions[0].c2+ '</ul>');
+//    Body.insertAdjacentHTML('beforeend', '<ul class = "choice">' +questions[0].c3+ '</ul>');
+//    Body.insertAdjacentHTML('beforeend', '<ul class = "choice">' +questions[0].c4+ '</ul>');
+//})
